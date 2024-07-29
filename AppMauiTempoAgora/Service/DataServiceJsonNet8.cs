0a0f342644c223440cfc9f99718e50e885ffdd78
Usuario <Usuario@CPD03-M20>
2024-07-29 12:16:21 -0300

    Começo do Projeto

---
+++ AppMauiTempoAgora/Service/DataServiceJsonNet8.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMauiTempoAgora.Service
+{
+    internal class DataServiceJsonNet8
+    {
+    }
+}
